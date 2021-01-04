4dd18fe7829d7de21ee29ed0b82633b4304f98fb
dominicmkennedy <61887814+dominicmkennedy@users.noreply.github.com>
2021-01-04 15:47:53 -0500

    Add files via upload

---
+++ action.php
@@ -0,0 +1,19 @@
+<html>
+<body>
+
+
+Home team: <?php echo $_POST["hteam"]; ?> Score: <?php echo $_POST["hscore"]; ?><br>
+Away team: <?php echo $_POST["ateam"]; ?> Score: <?php echo $_POST["ascore"]; ?><br>
+League: <?php echo $_POST["league"]; ?><br>
+Level: <?php echo $_POST["level"]; ?><br>
+Game Number: <?php echo $_POST["gnum"]; ?><br>
+Game Date: <?php echo $_POST["gdate"]; ?><br>
+Referee: <?php echo $_POST["ref"]; ?> Grade: <?php echo $_POST["refgrade"]; ?><br>
+Assistant Referee 1: <?php echo $_POST["ar1"]; ?> Grade: <?php echo $_POST["ar1grade"]; ?><br>
+Assistant Referee 2: <?php echo $_POST["ar2"]; ?> Grade: <?php echo $_POST["ar2grade"]; ?><br>
+Fourth Official: <?php echo $_POST["fourth"]; ?> Grade: <?php echo $_POST["fourthgrade"]; ?><br>
+
+
+
+</body>
+</html>
